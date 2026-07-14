81580c7dc4460781204d76dfcb3dcad17427bc3e
greenbank <greenbank-336@users.noreply.github.com>
2026-07-14 23:52:22 +0700

    job_applu .py

--- job_apply .py
+++ job_apply .py
@@ -1,17 +1,17 @@
 name = input("enter your name: ")
 age = int(input("enter your age: "))
 height = int(input("enter your height: "))
-strenght = int(input("enter your stenght from 1-10: "))
+strength = int(input("enter your stenght from 1-10: "))
 starter_pack = int(input("all the money your have : "))
 print("from my decision you areeee!!!!")
-if strenght  >= 8 and starter_pack >= 10000:
+if strength  >= 8 and starter_pack >= 10000:
     print("you are accepted!")
-else:
-    print("your fail get outttttttttt")
 print("your position is")
-if strenght == 8:
+if strength == 8:
    print("you are fatui harbinger class")
-elif strenght == 9 :
+elif strength == 9 :
     print("you are archon class")
-elif strenght == 10:
+elif strength == 10:
     print("you are sovereign class")
+else:
+    print("sorry your fail there is no position for you now Getttttt outtttt")
